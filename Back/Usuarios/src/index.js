c001f668b2fa862d7666d67df81dba1d193b880c
Becerra1226 <sebastian.becerra@uao.edu.co>
2025-05-22 16:06:40 -0500

    eliminar usuario

--- Back/Usuarios/src/index.js
+++ Back/Usuarios/src/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const cors = require('cors');
 const usuariosController = require('./controllers/usuariosController.js');
 const morgan = require('morgan');
+
 const app = express();
+
 app.use(morgan('dev'));
+app.use(cors({
+  origin: 'http://localhost',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  credentials: true,
+}));
 app.use(express.json());
 app.use(usuariosController);
-6
+app.use(cors());
+
+
 app.listen(3005, () => {
- console.log('backUsuarios ejecutandose en el puerto 3005');
+  console.log('backUsuarios ejecutandose en el puerto 3005');
 });
